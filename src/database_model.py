085d3a9388a69d9aeaa1f33b43bca89aa9387f05
wilsonnater <nwilson@enthought.com>
2025-03-20 12:29:47 -0500

    Migration example

--- src/database_model.py
+++ src/database_model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 # Define the base for declarative models
 Base = declarative_base()
@@ -12,6 +13,17 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     age = Column(Integer)
+    job_id = Column(Integer, ForeignKey('job.id'))
+    job = relationship("Job", back_populates="user")
 
     def __repr__(self):
         return f"<User(name='{self.name}', age={self.age})>"
+
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    permission_level = Column(Integer)
+
+    user = relationship("User", back_populates="job")
